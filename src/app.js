f6fdf51e009c516816edbfb424e273793b7ec6e1
Antelmo Verdugo <antelmoverdugo@gmail.com>
2026-01-20 18:27:51 -0700

    limiter implemented

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import routes from './routes/index.js';
 const app = express();
 app.use(cors());
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 20,
+})
+
+app.use(limiter);
+
 app.get('/', (req, res) => {
   res.send('Weather API by Antelmo Verdugo');
 });
